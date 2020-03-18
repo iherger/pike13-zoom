a884736076e96a189e94fe9721bfcac3cd490052
Ivan Herger <ivan.herger@gmail.com>
2020-03-18 09:30:49 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import moment from "moment";
 
 const config = {
-  pike13: {
+  pike: {
     accessToken: process.env.PIKE_ACCESS_TOKEN,
     baseUrl: process.env.PIKE_BASE_URL,
     serviceIds: process.env.PIKE_SERVICE_IDS
@@ -23,66 +23,96 @@ const randomIntFromInterval = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-// cron.schedule("* * * * *", () => {
+const pike = axios.create({
+  baseURL: config.pike.baseUrl,
+  headers: {
+    Authorization: `Bearer ${config.pike.accessToken}`,
+    "Content-Type": "application/json"
+  }
+});
+
+const zoom = axios.create({
+  baseURL: config.zoom.baseUrl,
+  headers: { Authorization: `Bearer ${config.zoom.jwt}` }
+});
+
+// main
 (async () => {
   console.log(config.users);
 
-  const pike13 = axios.create({
-    baseURL: config.pike13.baseUrl,
-    headers: { Authorization: `Bearer ${config.pike13.accessToken}` }
-  });
-
-  const zoom = axios.create({
-    baseURL: config.zoom.baseUrl,
-    headers: { Authorization: `Bearer ${config.zoom.jwt}` }
-  });
-
-  // get events
-  const events = await pike13.get("/event_occurrences", {
+  // get pike13 events
+  const events = await pike.get("/event_occurrences", {
     params: {
       from: moment().format("YYYY-MM-DD"),
       to: moment()
         .add(5, "days")
         .format("YYYY-MM-DD"),
-      service_ids: config.pike13.serviceIds
+      service_ids: config.pike.serviceIds
     }
   });
 
   const eventList = events.data.event_occurrences;
   console.log(eventList);
 
+  const note = await pike.post(
+    `/event_occurrences/${eventList?.[0].id}/notes`,
+    {
+      note: {
+        note: "The URL for the Online class is https://something.or.other",
+        public: true,
+        send_notifications: true
+      },
+      send_notifications: true
+    }
+  );
+  console.log(note.data);
+
+  // also send note to teacher
+
+  const singleEvent = await pike.get(`/event_occurrences/${eventList?.[0].id}`);
+  console.log(singleEvent.data);
+
   // get zoom users
   const users = await zoom.get("/users");
   console.log(users.data);
+  // match zoom users with event instructors (maybe)
 
-  // match zoom users with event instructors
+  // check if pike13 meetings have already been scheduled
+
+  // if pike13 meeting is
 
   //   create meeting
-  const createOptions = {
-    topic: "Online Yoga",
-    password: randomIntFromInterval(10000, 999999),
-    type: 2,
-    start_time: eventList?.[0].start_at,
-    timezone: eventList?.[0].timezone,
-    duration: 90,
-    agenda: `ID:${eventList?.[0].id}`
-  };
-  console.log(createOptions);
+  //   const createOptions = {
+  //     topic: eventList?.[0].name,
+  //     password: randomIntFromInterval(10000, 999999),
+  //     type: 2,
+  //     start_time: eventList?.[0].start_at,
+  //     timezone: eventList?.[0].timezone,
+  //     duration: 90,
+  //     agenda: `pike13Id:${eventList?.[0].id}`,
+  //     tracking_fields: [{ field: "pike13Id", value: eventList?.[0].id }]
+  //   };
+  //   console.log(createOptions);
 
-  const createMeeting = await zoom.post(
-    `/users/${config.users?.[0].zoom}/meetings`,
-    createOptions
-  );
-  console.log(createMeeting.data);
-
-  // get zoom meetings
-  const meetings = await zoom.get("/users/me/meetings");
-  console.log(meetings.data);
+  //   const createMeeting = await zoom.post(
+  //     `/users/${config.users?.[0].zoom}/meetings`,
+  //     createOptions
+  //   );
+  //   console.log(createMeeting.data);
 
   // check if meeting already exists for
 
   //
 
+  // get zoom meetings
+  const meetings = await zoom.get("/users/me/meetings", {
+    params: { type: "upcoming" }
+  });
+  console.log(meetings.data);
+
   //
 })();
-// });
+
+cron.schedule("* * * * *", () => {
+  console.log("Another minute has passed");
+});
