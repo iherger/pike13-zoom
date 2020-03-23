203df8e6e0d002dcc538ec61a7d3f7fc3a6845d6
Ivan Herger <ivan.herger@gmail.com>
2020-03-23 21:33:55 +0100

    Loop through funds, create meeting, create notification

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,19 @@ import * as cron from "node-cron";
 import axios from "axios";
 import moment from "moment";
 
+interface ZoomMeeting {
+  uuid: string;
+  id: number;
+  host_id: string;
+  topic: string;
+  type: number;
+  start_time: Date;
+  duration: number;
+  timezone: string;
+  created_at: Date;
+  join_url: string;
+}
+
 const config = {
   pike: {
     accessToken: process.env.PIKE_ACCESS_TOKEN,
@@ -14,7 +27,11 @@ const config = {
   },
   users: process.env.USER_IDS?.split(";").map(user => {
     const details = user.split(",");
-    return { name: details[0], pike: details[1], zoom: details[2] };
+    return {
+      name: details[0],
+      pike: parseInt(details[1], 10),
+      zoom: details[2]
+    };
   })
 };
 
@@ -37,82 +54,119 @@ const zoom = axios.create({
 });
 
 // main
-(async () => {
-  console.log(config.users);
+cron.schedule("* * * * *", async () => {
+  // (async () => {
+  console.log("\nStarting cron job...");
 
   // get pike13 events
-  const events = await pike.get("/event_occurrences", {
+  const eventsResponse = await pike.get("/event_occurrences", {
     params: {
-      from: moment().format("YYYY-MM-DD"),
+      from: moment().format(),
       to: moment()
-        .add(5, "days")
-        .format("YYYY-MM-DD"),
+        .add(24, "hours")
+        .format(),
       service_ids: config.pike.serviceIds
     }
   });
-
-  const eventList = events.data.event_occurrences;
-  console.log(eventList);
-
-  const note = await pike.post(
-    `/event_occurrences/${eventList?.[0].id}/notes`,
-    {
-      note: {
-        note: "The URL for the Online class is https://something.or.other",
-        public: true,
-        send_notifications: true
-      },
-      send_notifications: true
-    }
-  );
-  console.log(note.data);
-
-  // also send note to teacher
-
-  const singleEvent = await pike.get(`/event_occurrences/${eventList?.[0].id}`);
-  console.log(singleEvent.data);
-
-  // get zoom users
-  const users = await zoom.get("/users");
-  console.log(users.data);
-  // match zoom users with event instructors (maybe)
-
-  // check if pike13 meetings have already been scheduled
-
-  // if pike13 meeting is
-
-  //   create meeting
-  //   const createOptions = {
-  //     topic: eventList?.[0].name,
-  //     password: randomIntFromInterval(10000, 999999),
-  //     type: 2,
-  //     start_time: eventList?.[0].start_at,
-  //     timezone: eventList?.[0].timezone,
-  //     duration: 90,
-  //     agenda: `pike13Id:${eventList?.[0].id}`,
-  //     tracking_fields: [{ field: "pike13Id", value: eventList?.[0].id }]
-  //   };
-  //   console.log(createOptions);
-
-  //   const createMeeting = await zoom.post(
-  //     `/users/${config.users?.[0].zoom}/meetings`,
-  //     createOptions
-  //   );
-  //   console.log(createMeeting.data);
-
-  // check if meeting already exists for
-
-  //
+  const events = eventsResponse.data.event_occurrences;
+  console.log(`--Found ${events.length} events within the next 24 hours`);
 
   // get zoom meetings
-  const meetings = await zoom.get("/users/me/meetings", {
+  const meetingsResponse = await zoom.get("/users/me/meetings", {
     params: { type: "upcoming" }
   });
-  console.log(meetings.data);
+  const meetings = meetingsResponse.data.meetings as ZoomMeeting[];
+
+  // loop through upcoming pike13
+  for (let event of events) {
+    const singleEventResult = await pike.get(`/event_occurrences/${event.id}`);
+    const singleEvent = singleEventResult.data.event_occurrences[0];
+
+    const staff = config.users?.find(
+      user => user.pike === singleEvent.staff_members[0].id
+    );
+
+    // check if zoom meeting already exists
+    const singleZoomMeeting = meetings.find(
+      meeting => meeting.start_time === event.start_at
+    );
+
+    let url = singleZoomMeeting?.join_url;
+
+    if (!singleZoomMeeting) {
+      // create new zoom meeting
+      const createOptions = {
+        topic: event.name,
+        password: randomIntFromInterval(10000, 999999),
+        type: 2,
+        start_time: event.start_at,
+        timezone: event.timezone,
+        duration: 90,
+        agenda: `pike13Id:${event.id}`,
+        tracking_fields: [{ field: "pike13Id", value: event.id }],
+        settings: {
+          alternative_hosts: staff?.zoom
+        }
+      };
+
+      const createMeeting = await zoom.post(
+        `/users/${config.users?.[0].zoom}/meetings`,
+        createOptions
+      );
+      url = createMeeting.data.join_url;
+
+      console.log(
+        `----Created meeting at ${moment(event.start_at).format(
+          "YYYY-MM-DD HH:mm"
+        )}`
+      );
+    } else {
+      console.log(
+        `----Meeting at ${moment(event.start_at).format(
+          "YYYY-MM-DD HH:mm"
+        )} already exists`
+      );
+    }
+
+    // check if notice needs to be sent out
+    const now = moment();
+    const timeDifference = moment(event.start_at).diff(now, "minutes");
+
+    if (timeDifference <= 30) {
+      // check if note with link exists (load all notes for event occurrence, )
+      const existingNotesResult = await pike.get(
+        `/event_occurrences/${event.id}/notes`
+      );
+      const existingNotes = existingNotesResult.data.notes;
+
+      if (!existingNotes.length) {
+        const note = `Der Link für die Online Yoga Lektion von heute ${moment(
+          event.start_at
+        ).format(
+          "HH:mm"
+        )} Uhr lautet:<br /><a href="${url}" target="_blank">${url}</a>`;
+
+        await pike.post(`/event_occurrences/${event.id}/notes`, {
+          note: {
+            note,
+            public: true,
+            send_notifications: true
+          },
+          send_notifications: true
+        });
+        console.log(`----Created notification with meeting URL ${url}.`);
+      } else {
+        console.log("----Notification already exists. Skipping.");
+      }
+    } else {
+      console.log(
+        `----Too early for notification (${timeDifference} minutes).`
+      );
+    }
+  }
+
+  console.log("Finished cron job!");
 
   //
-})();
-
-cron.schedule("* * * * *", () => {
-  console.log("Another minute has passed");
+  // })();
 });
