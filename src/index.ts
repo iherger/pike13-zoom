9e3dda4485a966a68ede50ca5a882bed56fe6b75
Ivan Herger <ivan.herger@gmail.com>
2020-08-13 06:30:38 +0200

    Zoom with default user

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import * as cron from "node-cron";
 import axios from "axios";
 import moment from "moment-timezone";
-import { v4 } from "uuid";
+
+interface ZoomMeeting {
+  uuid: string;
+  id: number;
+  host_id: string;
+  topic: string;
+  type: number;
+  start_time: Date;
+  duration: number;
+  timezone: string;
+  created_at: Date;
+  join_url: string;
+}
 
 const config = {
   pike: {
@@ -9,15 +21,24 @@ const config = {
     baseUrl: process.env.PIKE_BASE_URL,
     serviceIds: process.env.PIKE_SERVICE_IDS,
   },
-  jitsi: {
-    baseUrl: process.env.JITSI_BASE_URL,
+  zoom: {
+    baseUrl: process.env.ZOOM_BASE_URL,
+    jwt: process.env.ZOOM_JWT,
+    user: process.env.ZOOM_USER,
   },
-  users: process.env.USER_IDS?.split(";").map((user) => {
-    const details = user.split(",");
-    return {
-      name: details[0],
-    };
-  }),
+  // users: process.env.USER_IDS?.split(";").map((user) => {
+  //   const details = user.split(",");
+  //   return {
+  //     name: details[0],
+  //     pike: parseInt(details[1], 10),
+  //     zoom: details[2],
+  //   };
+  // }),
+};
+
+const randomIntFromInterval = (min: number, max: number) => {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 const pike = axios.create({
@@ -28,8 +49,13 @@ const pike = axios.create({
   },
 });
 
+const zoom = axios.create({
+  baseURL: config.zoom.baseUrl,
+  headers: { Authorization: `Bearer ${config.zoom.jwt}` },
+});
+
 // main
-cron.schedule("*/5 * * * *", async () => {
+cron.schedule("*/10 * * * *", async () => {
   // (async () => {
   console.log("\nStarting cron job...");
 
@@ -44,10 +70,63 @@ cron.schedule("*/5 * * * *", async () => {
   const events = eventsResponse.data.event_occurrences;
   console.log(`--Found ${events.length} events within the next 24 hours`);
 
+  // get zoom meetings
+  const meetingsResponse = await zoom.get("/users/me/meetings", {
+    params: { type: "upcoming" },
+  });
+  const meetings = meetingsResponse.data.meetings as ZoomMeeting[];
+
   // loop through upcoming pike13
   for (let event of events) {
     const singleEventResult = await pike.get(`/event_occurrences/${event.id}`);
     const singleEvent = singleEventResult.data.event_occurrences[0];
+
+    // const staff = config.users?.find(
+    //   (user) => user.pike === singleEvent.staff_members[0].id
+    // );
+
+    // check if zoom meeting already exists
+    const singleZoomMeeting = meetings.find(
+      (meeting) =>
+        meeting.start_time === event.start_at && meeting.topic === event.name
+    );
+
+    let url = singleZoomMeeting?.join_url;
+
+    if (!singleZoomMeeting) {
+      // create new zoom meeting
+      const createOptions = {
+        topic: event.name,
+        password: randomIntFromInterval(10000, 999999),
+        type: 2,
+        start_time: event.start_at,
+        timezone: event.timezone,
+        duration: 90,
+        agenda: `pike13Id:${event.id}`,
+        tracking_fields: [{ field: "pike13Id", value: event.id }],
+        // settings: {
+        //   alternative_hosts: staff?.zoom,
+        // },
+      };
+
+      const createMeeting = await zoom.post(
+        `/users/${config.zoom.user}/meetings`,
+        createOptions
+      );
+      url = createMeeting.data.join_url;
+
+      console.log(
+        `----Created meeting at ${moment(event.start_at)
+          .tz(event.timezone)
+          .format("YYYY-MM-DD HH:mm")}`
+      );
+    } else {
+      console.log(
+        `----Meeting at ${moment(event.start_at)
+          .tz(event.timezone)
+          .format("YYYY-MM-DD HH:mm")} already exists`
+      );
+    }
 
     // check if notice needs to be sent out
     const now = moment();
@@ -61,8 +140,6 @@ cron.schedule("*/5 * * * *", async () => {
       const existingNotes = existingNotesResult.data.notes;
 
       if (!existingNotes.length) {
-        const url = `${config.jitsi.baseUrl}/Iyengar-Yoga-Zug-${v4()}`;
-
         const note = `Der Link für ${event.name} heute um ${moment(
           event.start_at
         )
